3fff8cc57a929487e79978cb11c26fab04893869
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2025-01-10 17:09:47 +0000

    Dietitian profile and Dietitian page's, Build fix.  (#5)
    * Update next.config.ts
    
    * Update nextjs.yml
    
    * Update nextjs_v4.yml
    
    * Remove TS shenanigans
    
    * Fix SSR fetching
    
    Co-Authored-By: Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
    Co-Authored-By: Hazal Kaynak <37582563+hazalmaslan@users.noreply.github.com>
    
    * Example IMG url config
    
    * Change dietitian endpoint
    
    Co-Authored-By: Hazal Kaynak <37582563+hazalmaslan@users.noreply.github.com>
    Co-Authored-By: Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
    
    * Fix additional TS errors
    
    Co-Authored-By: Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
    
    * Dietitian Profile Page overhaul
    
    * Register Page eslint fix
    
    * Fixes for build
    
    * generateStaticParams added
    
    ---------
    
    Co-authored-by: Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
    Co-authored-by: Hazal Kaynak <37582563+hazalmaslan@users.noreply.github.com>

--- src/app/dietitian/[username]/page.tsx
+++ src/app/dietitian/[username]/page.tsx
@@ -15,7 +15,29 @@ interface Profile {
   profile_picture?: string;
 }
 
-// Fetch dietitian profile data from the API
+//Fetch all the Dietians then get their usernames for generating static path for the generateStaticParams function.
+const fetchAllDietitians = async (): Promise<{ username: string }[]> => {
+  try {
+    const response = await axios.get(
+      "https://hazalkaynak.pythonanywhere.com/dietitian/"
+    );
+
+    if (response.data && Array.isArray(response.data.dietician_list)) {
+      return response.data.dietician_list.map(
+        (dietitian: { username: string }) => ({
+          username: dietitian.username,
+        })
+      );
+    } else {
+      console.error("Unexpected API response format.");
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching dietitian list:", error);
+    return [];
+  }
+};
+
 const fetchDietitianProfile = async (
   username: string
 ): Promise<Profile | null> => {
@@ -36,16 +58,25 @@ const fetchDietitianProfile = async (
   }
 };
 
+
+// Generate Static Params for Dynamic Route
+export const generateStaticParams = async () => {
+  const dietitians = await fetchAllDietitians();
+  return dietitians.map((dietitian) => ({
+    username: dietitian.username,
+  }));
+};
+
 // Main Profile Page Component
-type Params = Promise<{ username: string }>;
 export default async function DietitianProfilePage({
-  params,
+  params: paramsPromise,
 }: {
-  params: Params;
+  params: Promise<{ username: string }>;
 }) {
-  const { username } = await params; //Cutting Edge fix
+  const { username } = await paramsPromise; // Await here fixes my hopes
   const profile = await fetchDietitianProfile(username);
 
+  // Redirect to 404 if profile is not found
   if (!profile) {
     return notFound();
   }
